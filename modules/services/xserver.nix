c13c6347cca2872c5043b6115bc8ce3065ffdcec
ar175-lol <ghailmbv@proton.me>
2026-06-18 21:04:31 +0500

    refactor(services): split lightdm and xserver settings

--- modules/services/xserver.nix
+++ modules/services/xserver.nix
@@ -2,17 +2,10 @@ _: {
   nixos.desktop = {pkgs, ...}: {
     services.xserver = {
       enable = true;
-      displayManager.lightdm.enable = true;
       excludePackages = [pkgs.xterm];
       xkb = {
         layout = "us,ru";
         options = "grp:alt_shift_toggle";
-      };
-    };
-    services.displayManager = {
-      autoLogin = {
-        enable = true;
-        user = "ar175";
       };
     };
 
