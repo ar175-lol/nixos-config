f8b6e569bf02575be1f03067205e258441fec32a
ar175-lol <ghailmbv@proton.me>
2026-06-18 19:26:44 +0500

    feat(zsh): add abbr instead of aliases

--- modules/cli/zsh.nix
+++ modules/cli/zsh.nix
@@ -7,6 +7,7 @@ _: {
         enable = true;
         enableCompletion = true;
         syntaxHighlighting.enable = true;
+        autocd = true;
 
         autosuggestion = {
           enable = true;
@@ -19,18 +20,24 @@ _: {
           searchDownKey = "^[[B";
         };
 
+        zsh-abbr = {
+          enable = true;
+          abbreviations = {
+            update = "git add . && nh os switch";
+            upgrade = "git add . && nix flake update && nh os switch";
+            ncg = "nh clean all";
+          };
+        };
+
         initContent = ''
           echo -ne '\e[5 q'
 
           bindkey '^H' backward-kill-word
           bindkey "^[[1;5D" backward-word
           bindkey "^[[1;5C" forward-word
-
-          alias update="git add . && sudo nixos-rebuild switch --flake .#victus"
-          alias upgrade="git add . && nix flake update && sudo nixos-rebuild switch --flake .#victus"
-          alias ncg="nix-collect-garbage -d"
         '';
       };
+
       nix-index = {
         enable = true;
         enableZshIntegration = true;
