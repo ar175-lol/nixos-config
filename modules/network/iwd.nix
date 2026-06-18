944583b719fca8ba1a076f760e2e406b7283a508
ar175-lol <ghailmbv@proton.me>
2026-06-18 23:11:28 +0500

    feat(iwd): add other settings

--- modules/network/iwd.nix
+++ modules/network/iwd.nix
@@ -10,6 +10,18 @@ _: {
             EnableIPv6 = false;
             NameResolvingService = "none";
           };
+          General = {
+            EnableNetworkConfiguration = true;
+            AddressRandomization = "network";
+            AddressRandomizationRange = "full";
+            ManagementFrameProtection = "1";
+            ControlPortOverNL80211 = true;
+            DisableANQP = true;
+          };
+          Rank = {
+            BandModifier2_4GHz = 0.5;
+            BandModifier5GHz = 1.5;
+          };
           Settings = {
             AutoConnect = true;
           };
