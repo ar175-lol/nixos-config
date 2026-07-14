74d6f6415e438ef2083bc8a44a478cbff76f82f1
ar175-lol <ghailmbv@proton.me>
2026-07-14 19:25:59 +0500

    finally fix live folders

--- modules/apps/zen.nix
+++ modules/apps/zen.nix
@@ -9,6 +9,11 @@
       policies = import ./_policies.nix;
 
       profiles.default = {
+        settings = {
+          "zen.view.compact.enable-at-startup" = true;
+          "zen.window-sync.enabled" = true;
+        };
+
         mods = [
           "1b88a6d1-d931-45e8-b6c3-bfdca2c7e9d6"
           "a6335949-4465-4b71-926c-4a52d34bc9c0"
@@ -17,27 +22,38 @@
 
         pinsForce = true;
         pinsForceAction = "remove";
-        pins = {
-          "Youtube" = {
-            id = "6cd1b145-0dbf-40d7-8cf3-f5349abd67f1";
-            url = "https://youtube.com";
-            position = 101;
-            isEssential = true;
-          };
-          "GitHub" = {
-            id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
-            url = "https://github.com";
-            position = 102;
-            isEssential = true;
-          };
-        };
 
-        liveFolders = {
-          "Pull Requests" = {
-            id = "b7a3d5c1-9e2f-4a68-b0d4-6f1c8e5a2d93";
-            kind = "github:pull-requests";
-            position = 401;
-            github.authorMe = true;
+        spacesForce = true;
+        spaces = {
+          "Main" = {
+            id = "572910e1-4468-4832-a869-0b3a93e2f165";
+            position = 1000;
+
+            pins = {
+              "Youtube" = {
+                id = "6cd1b145-0dbf-40d7-8cf3-f5349abd67f1";
+                url = "https://youtube.com";
+                position = 101;
+                isEssential = true;
+              };
+              "GitHub" = {
+                id = "48e8a119-5a14-4826-9545-91c8e8dd3bf6";
+                url = "https://github.com";
+                position = 102;
+                isEssential = true;
+              };
+            };
+
+            liveFolders = {
+              "Pull Requests" = {
+                id = "b7a3d5c1-9e2f-4a68-b0d4-6f1c8e5a2d93";
+                kind = "github:pull-requests";
+                position = 401;
+                github = {
+                  authorMe = true;
+                };
+              };
+            };
           };
         };
 
