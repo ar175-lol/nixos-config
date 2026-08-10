92f648a46813456333b8e08185e443f4b96c942d
ar175-lol <ghailmbv@proton.me>
2026-08-10 19:45:37 +0500

    periodic commit

--- modules/hosts/victus/configuration.nix
+++ modules/hosts/victus/configuration.nix
@@ -2,7 +2,7 @@ _: {
   nixos.victus = {pkgs, ...}: {
     networking.hostName = "victus";
 
-    sops.age.sshKeyPaths = [ "/home/ar175/.ssh/id_ed25519" ];
+    sops.age.sshKeyPaths = ["/home/ar175/.ssh/id_ed25519"];
 
     environment.systemPackages = with pkgs; [
       kdePackages.kdenlive
